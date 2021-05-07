ea103d7727026ce075c503ffae0cb3994fdcd0b4
dev-pyun <chaebum.pyun@gmail.com>
2021-05-07 23:05:33 +0900

    C 언어로 Hello world! 파일을 만들어 실행함.
    C언어 실행을 위해 Visual Studio Community의 기본설정(win 32, 글꼴 등)을 마치고,
    hello world를 실행하는데 사용되는 헤더(<stdio.h>), 함수(int main(), printf()), return에 대해 배워봄.
    이때 main은 C 언어 파일에 반드시 있어야하는 존재임!

---
+++ hello/hello/hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h> //printf를 사용하기 위한 헤더파일
+
+int main()		 //뒤에 괄호가 붙은 녀석을 함수(function)이라고 함.
+/*C 파일에 int main()이 없으면 컴파일이 되지 않음.
+main함수가 반드시 있어야 하며, main이 먼저 실행됨.*/
+{
+	printf("Hello, world!\n");
+
+	return 0;		//반환값 (python에서도 배웠쥬? python에서는 정수 0을 제외한 나머지가 모두 True로 인식)
+}
