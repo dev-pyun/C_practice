c2a9c1aa5004280827a88b2ab6452a8814b35033
dev-pyun <chaebum.pyun@gmail.com>
2021-05-08 23:07:50 +0900

    C언어의 문자열 출력법과 서식 지정자를 이용하는 법을 학습.
    C언어의 경우, 문자열 출력법이 정해져 있고, python과는 다르게 항상 ';'(세미콜론)을 붙여줘야 한다는 특징이 있음. 이를 염두하고 잘 작성하기.
    또한, 서식 지정자의 경우, "%s %s", "Hello", "world!" 로 되어 있으면, %s는 문자열을 받는다는 소리이므로 Hello world! 로 출력됨.

--- hello/hello/hello.c
+++ hello/hello/hello.c
@@ -5,6 +5,9 @@ int main()		 //뒤에 괄호가 붙은 녀석을 함수(function)이라고 함.
 main함수가 반드시 있어야 하며, main이 먼저 실행됨.*/
 {
 	printf("Hello, world!\n");
+	printf("%s\n", "Hello, world!");
+	printf("%s %s\n", "Hello, world!", "1234");
+	printf("")
 
 	return 0;		//반환값 (python에서도 배웠쥬? python에서는 정수 0을 제외한 나머지가 모두 True로 인식)
 }
