a0d95d864fd7d0c50594c1d003fe7f7d1e0e559f
dev-pyun <chaebum.pyun@gmail.com>
2021-05-19 20:58:29 +0900

    정수 자료형에 대해 학습_1
    정수자료형의 종류와 그 범위, 만약 그 범위를 벗어날 때 나타나는 overflow와 underflow는 무엇인가에 대해 알아냄.

---
+++ 자료형 - 정수/자료형 - 정수/int 자료형.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    char num1 = 10;                         //1바이트 부호 있는 정수형으로 변수 선언과 동시에 값 할당
+    short num2 = 30000;                     //2바이트 부호 있는 정수형으로 변수 선언과 동시에 값 할당
+    int num3 = -1234567890;                 //3바이트 부호 있는 정수형으로 변수 선언과 동시에 값 할당
+    long num4 = 1234567890;                 //4바이트 부호 있는 정수형으로 변수 선언과 동시에 값 할당
+    long long num5 = -1234567890123456789;  //8바이트 부호 있는 정수형으로 변수 선언과 동시에 값 할당
+
+    unsigned char num6 = 200;                          // 1바이트 부호 없는 정수형으로 
+                                                   // 변수를 선언하고 값 할당
+
+    unsigned short num7 = 60000;                       // 2바이트 부호 없는 정수형으로 
+                                                       // 변수를 선언하고 값 할당
+
+    unsigned int num8 = 4123456789;                    // 4바이트 부호 없는 정수형으로 
+                                                       // 변수를 선언하고 값 할당
+
+    unsigned long num9 = 4123456789;                   // 4바이트 부호 없는 정수형으로 
+                                                       // 변수를 선언하고 값 할당
+
+    unsigned long long num10 = 12345678901234567890;    // 8바이트 부호 없는 정수형으로 
+                                                       // 변수를 선언하고 값 할당
+                                                       // 
+    // char, short, int는 %d로 출력하고 long은 %ld, long long은 %lld로 출력
+    printf("%d %d %d %ld %lld\n", num1, num2, num3, num4, num5);
+    printf("%u %u %u %lu %llu\n", num6, num7, num8, num9, num10);
+    return 0;
+}
