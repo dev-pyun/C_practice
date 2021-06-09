8daf72ba2e025c6abe12536a565d9919956078e9
dev-pyun <chaebum.pyun@gmail.com>
2021-06-09 22:10:49 +0900

    시험기간동안 복습

--- 상수/const.c
+++ 상수/const.c
@@ -5,6 +5,7 @@ int main()
     const int con1 = 1;
     const float con2 = 0.1f;
     const char con3 = 'a';
+    const long con4 = 20;
     //상수는 선언과 동시에 초기화를 해줘야 함.
     //상수에 초기화로 값을 할당한 순간 그 상수는 고정됨
 
