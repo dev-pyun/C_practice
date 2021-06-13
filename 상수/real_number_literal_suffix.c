d02cc3482e54fb41c6dd27c7d8af06fcf6ff400a
dev-pyun <chaebum.pyun@gmail.com>
2021-06-14 08:45:12 +0900

    상수 단원 학습 완료

---
+++ 상수/real_number_literal_suffix.c
@@ -0,0 +1,16 @@
+int main()
+{
+    printf("%f\n", 0.1f);
+    printf("%f\n", 0.1F);
+    printf("%f\n", 0.1);
+    printf("%Lf\n", 0.1l);
+    printf("%Lf\n", 0.1L);
+
+    printf("%f\n", 1.0e-5f);
+    printf("%f\n", 1.0e-5F);
+    printf("%f\n", 1.0e-5);
+    printf("%f\n", 1.0e-5l);
+    printf("%f\n", 1.0);
+
+    return 0;
+}
