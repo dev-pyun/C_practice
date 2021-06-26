219201b0956f26629603362133d15e41da1eb7f2
dev-pyun <chaebum.pyun@gmail.com>
2021-06-26 01:24:32 +0000

    ++, --라는 C언어만의 독특한 연산자에 대해 학습함.

---
+++ plus_minus/increment_integer.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1 = 1;
+    int num2 = 2;
+
+
+    num1++;     //num1 += 1
+    num2--;     //num2 -= 1
+
+    printf("%d %d", num1, num2);
+
+    return 0;
+}
