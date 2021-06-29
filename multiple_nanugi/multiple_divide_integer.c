fe44afc8a8e9defa1914b02cad3eb5db902f1281
dev-pyun <chaebum.pyun@gmail.com>
2021-06-29 21:29:07 +0900

    곱셈 나눗셈 학습함.
    실수형0을 나눗셈할 때에는 에러가 나지 않음.

---
+++ multiple_nanugi/multiple_divide_integer.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1;
+    int num2;
+    float num3;
+    float num4;
+
+    num1 = 2 * 3;
+    num2 = 7 / 2;
+
+    num3 = 2.73f * 3.14f;
+    num4 = 7.0f / 2.0f;
+
+    printf("%d %d\n", num1, num2);
+    printf("%f %f\n", num3, num4);
+
+    return 0;
+
+}
